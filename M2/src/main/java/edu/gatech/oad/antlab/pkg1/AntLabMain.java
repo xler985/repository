94b496a4fb31493e8711ca65376dbda3ab3e14d9
Sterling Velazquez <46730686+SterlingVelazquez@users.noreply.github.com>
2019-01-22 19:49:09 -0500

    Update AntLabMain.java

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,7 +73,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("gburdell4");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
+                  Person5 p5 = new Person5("Sterling");
                   toPrint += p5.toString("gburdell5");
 		  
         System.out.println(toPrint);
